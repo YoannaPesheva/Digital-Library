0f30c233e63bfaa95554bbe8f9fa5bdd106f19c7
YoannaPesheva <yoannapesheva6802@gmail.com>
2022-05-04 13:58:04 +0300

    just wanted to see how files work

--- main.cpp
+++ main.cpp
@@ -1,5 +1,29 @@
 #include<iostream>
+#include<fstream>
+#include<string>
+using namespace std;
+
+
 int main()
 {
-	std::cout << "pls work" << std::endl;
+	ofstream out("trying.txt");
+	if (out.is_open())
+		out << "Is it working?" << endl;
+	else out << "Apparently not" << endl;
+	out.close();
+
+	ifstream in;
+	string text = "";
+	in.open("trying.txt");
+	if (in.is_open())
+	{
+		while (getline(in, text))
+		{
+			cout << text;
+		}
+	}
+	in.close();
+
+	return 0;
+
 }
