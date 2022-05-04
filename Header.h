b368cd28dcf300e4339b93e83eff5b85eccb19ee
YoannaPesheva <yoannapesheva6802@gmail.com>
2022-05-04 10:19:06 +0300

    Created class Books + definition of the big 4

--- Header.h
+++ Header.h
@@ -1,1 +1,16 @@
 #pragma once
+class Books {
+private:
+	char* author;
+	char* title;
+	char* nameOfATextFile;
+	char* summary; //a short text 
+	double rating; //9.5
+	int ISBN; //long int? 
+public:
+	//Dynamically allocated memory =>
+	Books(); //default constructor
+	Books(const Books& other); //copy constructor
+	Books& operator=(const Books& other); //=
+	~Books(); //destructor
+};
